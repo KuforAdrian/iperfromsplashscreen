d8bedce4ed0588a3add1772949b8eda0167cf6d1
KuforAdrian <adrianteintsem@gmail.com>
2023-12-12 07:47:16 +0100

    Added all files

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -38,7 +40,7 @@ class MyAnimatedSplashScreen extends StatelessWidget {
        child: 
               AnimatedSplashScreen(
               splash: Image.asset('assets/ipeformlogo.png'),
-              nextScreen: HomePage(),
+              nextScreen: const HomePage(),
               splashTransition: SplashTransition.fadeTransition,
               duration: 10000,
               backgroundColor: Colors.transparent,
